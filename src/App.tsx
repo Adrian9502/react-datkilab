11ddffbe0afeee43e07c37328f3ed14d6e3c8741
Adrian <bontojohnadrian@gmail.com>
2024-11-26 21:14:47 +0800

    created starting files and created header component

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header"
+
+function App() {
+
+  return (
+    <main className="bg-orange-600 min-h-screen">
+    <Header/>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className='p-10 bg-pink-600 flex items-center justify-center'>Header</header>
+  )
+}
